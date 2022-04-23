fcab989be1a9f7b86ca97ae0c547177e8990dedf
BeenLi <wanli.99@qq.com>
2022-04-23 23:01:49 +0800

    update readme

--- common.py
+++ common.py
@@ -170,6 +170,18 @@ consts['tmp_prefix'] = 'tmp.'
 class ExitLoop(Exception):
     pass
 
+def _init():
+    global _global_dict
+    _global_dict = {}
+
+def set_value(key,value):
+    _global_dict[key] = value
+
+def get_value(key,defValue=None):
+    try:
+        return _global_dict[key]
+    except KeyError:
+        return defValue
 class LkmcCliFunction(cli_function.CliFunction):
     '''
     Common functionality shared across our CLI functions:
